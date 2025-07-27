cbee661d6cd1e2c78fb2f74b42d6d8dffe918a70
Zaven Jumayan <zavenjumayan@gmail.com>
2025-07-27 19:06:18 +0400

    Authenthication

---
+++ src/app/(home)/protected/page.tsx
@@ -0,0 +1,9 @@
+const Page=()=>{
+    return(
+        <div>
+            Only Logged in users can see how we are here
+        </div>
+    )
+}
+
+export default Page;
---
+++ src/modules/home/ui/components/home-sidebar/index.tsx
@@ -0,0 +1,17 @@
+import {Sidebar, SidebarContent} from "@/components/ui/sidebar";
+import {MainSection} from "@/modules/home/ui/components/home-sidebar/main-section";
+import {Separator} from "@/components/ui/separator";
+import {PersonaSection} from "@/modules/home/ui/components/home-sidebar/personal-section";
+
+export const HomeSidebar = () => {
+    return (
+        <Sidebar className="pt-16 z-40 border-none" collapsible="icon">
+            <SidebarContent className="bg-background">
+                <MainSection/>
+                <Separator/>
+                <PersonaSection/>
+            </SidebarContent>
+        </Sidebar>
+
+    )
+}
